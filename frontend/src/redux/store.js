8d77d5c5723e452a046af288d6753c40acde51bd
Vedant Lahane <vedantanillahane@gmail.com>
2025-04-21 22:38:57 +0530

    feat: Add wishlist routes and integrate wishlist reducer in the store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import productReducer from './slices/productSlice';
 import cartReducer from './slices/cartSlice';
+import wishlistReducer from './slices/wishlistSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     product: productReducer,
     cart: cartReducer,
+    wishlist: wishlistReducer,
   },
 });
 
