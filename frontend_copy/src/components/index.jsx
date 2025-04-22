c93f544829d35fab744ce1aceaec62e8200e59dc
Vedant Lahane <vedantanillahane@gmail.com>
2025-04-23 04:29:17 +0530

    Remove cart-related components and assets to streamline the application

--- frontend_copy/src/components/index.jsx
+++
@@ -1,5 +0,0 @@
-export { default as Navbar } from './Navbar.jsx';
-export {default as Cart} from './Cart.jsx'
-export { default as Home } from './Home.jsx';
-export { default as Sales } from './Sales.jsx';
-export { default as Footer } from './Footer.jsx';
